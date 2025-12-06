1b15b92b9aeed0042e8552b1b643e3e12fed8be2
Joseph Loper <jd.loper@icloud.com>
2025-12-06 00:22:29 -0700

    Add MedicalCode model class for managing code details

---
+++ src/main/java/MedicalCode.java
@@ -0,0 +1,34 @@
+public class MedicalCode {
+    private String code;
+    private String description;
+    private String type;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + code + " [" + type + "] - " + description;
+    }
+}
